926a06feb7adce85f59796ebced8d866f5aced2c
Whaletrucker Reef <scutua.tua@icloud.com>
2026-06-10 14:44:24 +0000

    fix: suppress ER_NO_SUCH_THREAD error log in killConnection

--- vendor/github.com/go-mysql-org/go-mysql/replication/binlogsyncer.go
+++ vendor/github.com/go-mysql-org/go-mysql/replication/binlogsyncer.go
@@ -979,8 +979,9 @@ func (b *BinlogSyncer) newConnection(ctx context.Context) (*client.Conn, error) 
 func (b *BinlogSyncer) killConnection(conn *client.Conn, id uint32) {
 	cmd := fmt.Sprintf("KILL %d", id)
 	if _, err := conn.Execute(cmd); err != nil {
-		b.cfg.Logger.Errorf("kill connection %d error %v", id, err)
 		// Unknown thread id
+		if code := ErrorCode(err.Error()); code != ER_NO_SUCH_THREAD {
+			b.cfg.Logger.Errorf("kill connection %d error %v", id, err)
 		if code := ErrorCode(err.Error()); code != ER_NO_SUCH_THREAD {
 			b.cfg.Logger.Error(errors.Trace(err))
 		}
